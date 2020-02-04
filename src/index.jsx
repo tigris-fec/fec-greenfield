03a73170693c1113db9ab3d085653f5126cd0bd5
SDFIW <asdfo2@SDFIWs-MacBook-Pro.local>
2020-02-04 14:28:34 -0500

    added wrapper for the index to integrate redux

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import {Provider} from 'react-redux';
 import "bulma";
 
 ReactDOM.render(
+  <Provider>
   <Router>
     <App />
-  </Router>,
+  </Router>
+  </Provider>,
   document.getElementById("root")
 );
