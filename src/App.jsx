c342f54591d260f3aa0a62f93796bfff51427530
SDFIW <asdfo2@SDFIWs-MacBook-Pro.local>
2020-02-07 09:13:41 -0500

    refactored the files so that the app properly works

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
 import RR from "./components/ratings-&-reviews/RR.jsx";
-import OverviewMock from "./components/overview/OverviewMock.jsx";
 import Overview from "./components/overview/Overview.jsx";
 import QA from './components/questions-&-answers/QA.jsx'
 
@@ -18,13 +17,11 @@ const App = () => {
       <br />
       <section className="section">
         <Switch>
-          <Route exact path="/example-list" render={() => <ExampleList />} />
-          <Route exact path="/example-form" render={() => <ExampleForm />} />
           <Route exact path="/overview" render={() => <Overview />} />
           <Route exact path="/questions" render={() => <QA />} />
+          <Route exact path="/ratings" render={() => <RR />} />
         </Switch>
       </section>
-      <RR />
     </div>
     </>
 
