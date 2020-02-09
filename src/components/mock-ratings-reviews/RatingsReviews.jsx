adc8fc9af151e554974de371ea3e05519ad3f3ed
Jarrod Chung <jarrodchung.dev@gmail.com>
2020-02-09 15:05:52 -0500

    Added breaks to file for readability

--- src/components/mock-ratings-reviews/RatingsReviews.jsx
+++ src/components/mock-ratings-reviews/RatingsReviews.jsx
@@ -125,106 +125,6 @@ const RatingsReviews = (props) => {
                 </div>
               </div>
             </div>
-            <div className="level" style={{ borderBottom: "solid", borderWidth: "1px" }}>
-              <div className="box is-shadowless is-marginless">
-                <div className="media" style={{ fontSize: "75%" }}>
-                  <div className="media-content">
-                    <div className="content">
-                      <div className="level">
-                        <div className="level-left">
-                          <div className="level-item">
-                            <Rating
-                              initialRating={2.45}
-                              emptySymbol={<i className="far fa-star"></i>}
-                              fullSymbol={
-                                <i
-                                  className="fa fa-star"
-                                  style={{ color: "#d4af37" }}
-                                ></i>
-                              }
-                            />
-                          </div>
-                        </div>
-
-                        <div className="level-right">
-                          <div className="level-item">
-                            <i className="far fa-check-circle"></i>
-                          </div>
-                          <div className="level-item">
-                            User1234 &nbsp;&nbsp; January 1, 2019
-                          </div>
-                        </div>
-                      </div>
-                      <div style={{ paddingBottom: "1.5%", fontSize: "125%" }}>
-                        <strong>Donut chocolate bar pudding.</strong>
-                      </div>
-                      <div style={{ paddingBottom: "1.5%" }}>...line if necessary</div>
-                      <p style={{ fontSize: "110%" }}>
-                        Only the chunkiest of boys can eat this donust chocolate bar
-                        pudding. Chocolate bar gingerbread sweet carrot cake.
-                      </p>
-                    </div>
-                    <div className="level">
-                      <div className="level-left">
-                        <div className="level-item">
-                          <p style={{ fontStyle: "bold" }}>
-                            <strong></strong>
-                          </p>
-                        </div>
-                        <div className="level-item">
-                          <div
-                            className="box is-shadowless is-paddingless is-marginless"
-                            style={{ backgroundColor: "#888888" }}
-                          >
-                            <div className="field is-grouped">
-                              <div className="tags has-addons">
-                                <div className="tag has-addons is-dark">✓</div>
-                                <div className="tag has-addons">
-                                  I recommend this product
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="level">
-                      <div
-                        className="notification"
-                        style={{
-                          backgroundColor: "#EBEBEB",
-                          borderRadius: "0%"
-                        }}
-                      >
-                        <strong>Response:</strong>
-                        <p>
-                          Marzipan danish jelly gummi beans apple pie cheesecake topping
-                          biscuit sesame snaps.
-                        </p>
-                      </div>
-                    </div>
-                    <div className="level">
-                      <div className="level-left">
-                        <div className="level-item">
-                          <p>Helpful?</p>
-                        </div>
-                        <div className="level-item">
-                          <p>
-                            <u>Yes</u> (9)
-                          </p>
-                        </div>
-                        <div className="level-item">|</div>
-                        <div className="level-item">
-                          <p>
-                            <u>Report</u>
-                          </p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
 
             <div className="level" style={{ borderBottom: "solid", borderWidth: "1px" }}>
               <div className="box is-shadowless is-marginless">
@@ -326,6 +226,108 @@ const RatingsReviews = (props) => {
                 </div>
               </div>
             </div>
+
+            <div className="level" style={{ borderBottom: "solid", borderWidth: "1px" }}>
+              <div className="box is-shadowless is-marginless">
+                <div className="media" style={{ fontSize: "75%" }}>
+                  <div className="media-content">
+                    <div className="content">
+                      <div className="level">
+                        <div className="level-left">
+                          <div className="level-item">
+                            <Rating
+                              initialRating={2.45}
+                              emptySymbol={<i className="far fa-star"></i>}
+                              fullSymbol={
+                                <i
+                                  className="fa fa-star"
+                                  style={{ color: "#d4af37" }}
+                                ></i>
+                              }
+                            />
+                          </div>
+                        </div>
+
+                        <div className="level-right">
+                          <div className="level-item">
+                            <i className="far fa-check-circle"></i>
+                          </div>
+                          <div className="level-item">
+                            User1234 &nbsp;&nbsp; January 1, 2019
+                          </div>
+                        </div>
+                      </div>
+                      <div style={{ paddingBottom: "1.5%", fontSize: "125%" }}>
+                        <strong>Donut chocolate bar pudding.</strong>
+                      </div>
+                      <div style={{ paddingBottom: "1.5%" }}>...line if necessary</div>
+                      <p style={{ fontSize: "110%" }}>
+                        Only the chunkiest of boys can eat this donust chocolate bar
+                        pudding. Chocolate bar gingerbread sweet carrot cake.
+                      </p>
+                    </div>
+                    <div className="level">
+                      <div className="level-left">
+                        <div className="level-item">
+                          <p style={{ fontStyle: "bold" }}>
+                            <strong></strong>
+                          </p>
+                        </div>
+                        <div className="level-item">
+                          <div
+                            className="box is-shadowless is-paddingless is-marginless"
+                            style={{ backgroundColor: "#888888" }}
+                          >
+                            <div className="field is-grouped">
+                              <div className="tags has-addons">
+                                <div className="tag has-addons is-dark">✓</div>
+                                <div className="tag has-addons">
+                                  I recommend this product
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="level">
+                      <div
+                        className="notification"
+                        style={{
+                          backgroundColor: "#EBEBEB",
+                          borderRadius: "0%"
+                        }}
+                      >
+                        <strong>Response:</strong>
+                        <p>
+                          Marzipan danish jelly gummi beans apple pie cheesecake topping
+                          biscuit sesame snaps.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="level">
+                      <div className="level-left">
+                        <div className="level-item">
+                          <p>Helpful?</p>
+                        </div>
+                        <div className="level-item">
+                          <p>
+                            <u>Yes</u> (9)
+                          </p>
+                        </div>
+                        <div className="level-item">|</div>
+                        <div className="level-item">
+                          <p>
+                            <u>Report</u>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
             <div className="level">
               <div className="level-left">
                 {buttons.map((button) => {
