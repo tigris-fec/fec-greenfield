c4bd6df8f7fe6ef727f2f513c986e554a84465a3
Jarrod Chung <jarrodchung.dev@gmail.com>
2020-02-09 06:17:14 -0500

    Cleaned up styling

--- src/components/ratings-reviews/RatingsReviews.jsx
+++ src/components/ratings-reviews/RatingsReviews.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
 import Rating from "react-rating";
+import { size, width, comfort, quality, length, fit } from "./state.js";
 import "./RatingsReviews.css";
 
 const RatingsReviews = (props) => {
+  const [widget] = useState("Ratings & Reviews");
   const [ratings] = useState([5, 4, 3, 2, 1]);
   const [filters] = useState(["relevance", "newest", "helpful"]);
-  const [buttons] = useState(["MORE REVIEWS", "ADD A REVIEW   +"]);
+  const [buttons] = useState(["More Reviews", "Add A Review +"]);
+  const [sizes] = useState(size);
+  const [widths] = useState(width);
+  const [comforts] = useState(comfort);
+  const [qualities] = useState(quality);
+  const [lengths] = useState(length);
+  const [fits] = useState(fit);
+
   return (
     <>
-      <div className="container" style={{ padding: "4%" }}>
+      <div className="container is-fluid">
         <div className="columns">
           <div className="column is-4">
-            RATINGS & REVIEWS
+            {widget.toUpperCase()}
             <div className="level">
               <div className="level-left">
                 <div className="level-item">
@@ -19,8 +28,9 @@ const RatingsReviews = (props) => {
                     <strong>3.5</strong>
                   </p>
                 </div>
-                <div id="ratings-stars" className="level-item">
+                <div className="level-item">
                   <Rating
+                    key={1}
                     initialRating={4.67}
                     emptySymbol={<i className="far fa-star"></i>}
                     fullSymbol={
@@ -41,25 +51,136 @@ const RatingsReviews = (props) => {
               <div className="level-left">
                 <div className="level-item">
                   <table id="star-counts" className="table">
-                    {ratings.map((rating) => {
-                      return (
-                        <tr>
-                          <td>
-                            <u>{rating} Stars</u>
-                          </td>
-                          <td>
-                            <progress value={25} max={100} />
-                          </td>
-                        </tr>
-                      );
-                    })}
+                    <tbody>
+                      {ratings.map((rating, index) => {
+                        return (
+                          <tr key={index}>
+                            <td>
+                              <u>{rating} Stars</u>
+                            </td>
+                            <td>
+                              <progress value={25} max={100} />
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
                   </table>
+                </div>
+              </div>
+            </div>
+
+            <div className="level" style={{ fontSize: "65%" }}>
+              <div className="level-left">
+                <div className="level-item">
+                  <div className="table-container">
+                    <table
+                      id="breakdown"
+                      className="table"
+                      style={{ borderColor: "white" }}
+                    >
+                      <tbody>
+                        <tr
+                          style={{
+                            backgroundColor: "#EBEBEB",
+                            border: "solid",
+                            borderColor: "white"
+                          }}
+                        >
+                          {sizes.map((fit, index) => {
+                            return index % 2 !== 0 ? (
+                              <td id={`size-${index.index}-`} key={index}>
+                                {null}
+                              </td>
+                            ) : (
+                              <td key={index}>{fit}</td>
+                            );
+                          })}
+                        </tr>
+                        <tr
+                          style={{
+                            backgroundColor: "#EBEBEB",
+                            border: "solid",
+                            borderColor: "white"
+                          }}
+                        >
+                          {widths.map((fit, index) => {
+                            return index % 2 !== 0 ? (
+                              <td key={index}>{null}</td>
+                            ) : (
+                              <td key={index}>{fit}</td>
+                            );
+                          })}
+                        </tr>
+                        <tr
+                          style={{
+                            backgroundColor: "#EBEBEB",
+                            border: "solid",
+                            borderColor: "white"
+                          }}
+                        >
+                          {comforts.map((fit, index) => {
+                            return index % 2 !== 0 ? (
+                              <td key={index}>{null}</td>
+                            ) : (
+                              <td key={index}>{fit}</td>
+                            );
+                          })}
+                        </tr>
+                        <tr
+                          style={{
+                            backgroundColor: "#EBEBEB",
+                            border: "solid",
+                            borderColor: "white"
+                          }}
+                        >
+                          {qualities.map((fit, index) => {
+                            return index % 2 !== 0 ? (
+                              <td key={index}>{null}</td>
+                            ) : (
+                              <td key={index}>{fit}</td>
+                            );
+                          })}
+                        </tr>
+                        <tr
+                          style={{
+                            backgroundColor: "#EBEBEB",
+                            border: "solid",
+                            borderColor: "white"
+                          }}
+                        >
+                          {lengths.map((fit, index) => {
+                            return index % 2 !== 0 ? (
+                              <td key={index}>{null}</td>
+                            ) : (
+                              <td key={index}>{fit}</td>
+                            );
+                          })}
+                        </tr>
+                        <tr
+                          style={{
+                            backgroundColor: "#EBEBEB",
+                            border: "solid",
+                            borderColor: "white"
+                          }}
+                        >
+                          {fits.map((fit, index) => {
+                            return index % 2 !== 0 ? (
+                              <td key={index}>{null}</td>
+                            ) : (
+                              <td key={index}>{fit}</td>
+                            );
+                          })}
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
 
-          <div className="column">
+          <div className="column is-8">
             <div className="content">
               <div className="level is-marginless">
                 <div className="level-left">
@@ -77,69 +198,100 @@ const RatingsReviews = (props) => {
                   </div>
                 </div>
               </div>
-
-              <div
-                className="level"
-                style={{ borderBottom: "solid", borderWidth: "1px" }}
-              >
-                <div className="box is-shadowless is-marginless">
-                  <div className="media" style={{ fontSize: "75%" }}>
-                    <div className="media-content">
-                      <div className="content">
-                        <div className="level">
-                          <div className="level-left">
-                            <div className="level-item">
-                              <Rating
-                                initialRating={4.67}
-                                emptySymbol={<i className="far fa-star"></i>}
-                                fullSymbol={
-                                  <i
-                                    className="fa fa-star"
-                                    style={{ color: "#d4af37" }}
-                                  ></i>
-                                }
-                              />
-                            </div>
-                          </div>
-                          <div className="level-right">
-                            <div className="level-item">
-                              <i className="far fa-check-circle"></i>
-                            </div>
-                            <div className="level-item">
-                              <p>User1234, January 1, 2019</p>
-                            </div>
-                          </div>
-                        </div>
-                        <div style={{ paddingBottom: "1.5%", fontSize: "125%" }}>
-                          <strong>
-                            Review title with word-break truncation to prevent mapping one
-                            to the next
-                          </strong>
-                        </div>
-                        <div style={{ paddingBottom: "1.5%" }}>...line if necessary</div>
-                        <p style={{ fontSize: "110%" }}>
-                          I swear to Bob, I'd give up my first born and the ability to
-                          love another human if I could have another taste of this sweet
-                          sweet goodness. Lemon drops cake pudding pudding.
-                        </p>
-                      </div>
-
+            </div>
+            <div className="level" style={{ borderBottom: "solid", borderWidth: "1px" }}>
+              <div className="box is-shadowless is-marginless">
+                <div className="media" style={{ fontSize: "75%" }}>
+                  <div className="media-content">
+                    <div className="content">
                       <div className="level">
                         <div className="level-left">
                           <div className="level-item">
-                            <p>Helpful?</p>
+                            <Rating
+                              initialRating={2.45}
+                              emptySymbol={<i className="far fa-star"></i>}
+                              fullSymbol={
+                                <i
+                                  className="fa fa-star"
+                                  style={{ color: "#d4af37" }}
+                                ></i>
+                              }
+                            />
+                          </div>
+                        </div>
+
+                        <div className="level-right">
+                          <div className="level-item">
+                            <i className="far fa-check-circle"></i>
                           </div>
                           <div className="level-item">
-                            <p>
-                              <u>Yes</u> (9)
-                            </p>
+                            User1234 &nbsp;&nbsp; January 1, 2019
                           </div>
-                          <div className="level-item">|</div>
-                          <div className="level-item">
-                            <p>
-                              <u>Report</u>
-                            </p>
+                        </div>
+                      </div>
+                      <div style={{ paddingBottom: "1.5%", fontSize: "125%" }}>
+                        <strong>Donut chocolate bar pudding.</strong>
+                      </div>
+                      <div style={{ paddingBottom: "1.5%" }}>...line if necessary</div>
+                      <p style={{ fontSize: "110%" }}>
+                        Only the chunkiest of boys can eat this donust chocolate bar
+                        pudding. Chocolate bar gingerbread sweet carrot cake.
+                      </p>
+                    </div>
+                    <div className="level">
+                      <div className="level-left">
+                        <div className="level-item">
+                          <p style={{ fontStyle: "bold" }}>
+                            <strong></strong>
+                          </p>
+                        </div>
+                        <div className="level-item">
+                          <div
+                            className="box is-shadowless is-paddingless is-marginless"
+                            style={{ backgroundColor: "#888888" }}
+                          >
+                            <div className="field is-grouped">
+                              <div className="tags has-addons">
+                                <div className="tag has-addons is-dark">✓</div>
+                                <div className="tag has-addons">
+                                  I recommend this product
+                                </div>
+                              </div>
+                            </div>
                           </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="level">
+                      <div
+                        className="notification"
+                        style={{
+                          backgroundColor: "#EBEBEB",
+                          borderRadius: "0%"
+                        }}
+                      >
+                        <strong>Response:</strong>
+                        <p>
+                          Marzipan danish jelly gummi beans apple pie cheesecake topping
+                          biscuit sesame snaps.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="level">
+                      <div className="level-left">
+                        <div className="level-item">
+                          <p>Helpful?</p>
+                        </div>
+                        <div className="level-item">
+                          <p>
+                            <u>Yes</u> (9)
+                          </p>
+                        </div>
+                        <div className="level-item">|</div>
+                        <div className="level-item">
+                          <p>
+                            <u>Report</u>
+                          </p>
                         </div>
                       </div>
                     </div>
@@ -157,7 +309,7 @@ const RatingsReviews = (props) => {
                         <div className="level-left">
                           <div className="level-item">
                             <Rating
-                              initialRating={4.67}
+                              initialRating={2.45}
                               emptySymbol={<i className="far fa-star"></i>}
                               fullSymbol={
                                 <i
@@ -168,12 +320,13 @@ const RatingsReviews = (props) => {
                             />
                           </div>
                         </div>
+
                         <div className="level-right">
                           <div className="level-item">
                             <i className="far fa-check-circle"></i>
                           </div>
                           <div className="level-item">
-                            <p>User1234, January 1, 2019</p>
+                            User1234 &nbsp;&nbsp; January 1, 2019
                           </div>
                         </div>
                       </div>
@@ -260,7 +413,7 @@ const RatingsReviews = (props) => {
                           borderRadius: "0"
                         }}
                       >
-                        {button}
+                        <strong>{button.toUpperCase()}</strong>
                       </div>
                     </div>
                   );
@@ -275,3 +428,19 @@ const RatingsReviews = (props) => {
 };
 
 export default RatingsReviews;
+
+/*
+
+
+Length
+
+
+
+Fit
+Runs tight
+Runs slightly tight
+Perfect
+Runs slightly long
+Runs long
+
+*/
