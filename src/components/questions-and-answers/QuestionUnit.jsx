7e6e86c429836074def818e1e9e5d843e95a6d43
gbzaid2 <55711452+gbzaid2@users.noreply.github.com>
2020-02-10 21:49:48 -0500

    Full functionality qa (#76)
    * Add question form validation and basic styling
    
    * Add functionality to More Questions button
    
    * Add upvote functionality
    
    * "remove import of deleted mockDataQuestions.js"
    
    * Format code using prettier
    
    * Format code with prettier
    
    * Load Question from API
    
    * Report functionality, hide more questions button, upvote enhancements
    
    * Complete API integration
    
    * Fill gaps in functionality and views, such as ordering questions/answers by helpfulness, displaying date, et
    
    * Complete full functional
    
    * BugFix on show more answers button
    
    * Add a bit of style

--- src/components/questions-and-answers/QuestionUnit.jsx
+++ src/components/questions-and-answers/QuestionUnit.jsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import AnswerUnit from './AnswerUnit.jsx'
-
-const handleClick = (e) => {
-    console.log(e);
-}
+import React, { useState } from "react";
+import Axios from 'axios';
+import AnswerUnit from "./AnswerUnit.jsx";
+import $ from 'jquery';
+import AddAnswer from "./AddAnswer.jsx"
 const QuestionUnit = (props) => {
+  const [votes, setVotes] = useState(props.question.question_helpfulness);
+  const [didVote, setDidVote] = useState(false);
+  const [answersToDisplay, setAnswersToDisplay] = useState(2);
+  const [displayAll, setDisplayAll] = useState(false);
+  const answers = props.question.answers;
+  const handleClick = function(id) {
+    if (didVote) return;
+    Axios.put(`http://3.134.102.30/qa/question/${id}/helpful`)
+    .then((data) => {console.log(data)})
+    .catch((err) => {console.log(err)});
+    setDidVote(true);
+    setVotes(votes + 1);
+  };
 
-    let answers = props.question.answers;
-    //console.log(answers);
-    return (
-        <div>
-            Q: {props.question.question_body}
-            Helpful?
-            <a href="#" onClick={handleClick}>Yes ({props.question.question_helpfulness})</a>
-            |
-            <a href= "#" onClick={handleClick}>Add Answer</a>
-           {Object.keys(answers).map((answerId,index) => <AnswerUnit answer = {answers[answerId]}/>) }
-        </div>
-    );
+  const handleAddAnswer = function() {
 
-}
+    props.setCurrentlyAnswering(props.question.question_id);
+    $("#answer-form").addClass("is-active");
+  };
+
+  return (
+    <div>
+      Q: {props.question.question_body}
+      Helpful?
+      <a onClick={() => {handleClick(props.question.question_id)}}>Yes ({votes})</a>|
+      <a onClick={handleAddAnswer}>Add Answer</a>
+      {answers.slice(0,answersToDisplay)
+        .map((answer, index) => (
+          <AnswerUnit answer={answer} key = {index}/>
+        ))}
+      
+      {answersToDisplay < answers.length ? (
+        <b onClick={() => setAnswersToDisplay(100)}>See More Answers</b>
+      ) : (
+        ""
+      )}
+      {answersToDisplay === 100 ? <b onClick = {() => setAnswersToDisplay(2)} >Collapse Answers</b>: ""}
+    </div>
+  );
+};
 
 export default QuestionUnit;
