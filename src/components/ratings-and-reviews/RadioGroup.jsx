4ef8713a99d69f3d828d0393fdfcb66610711dc8
Jarrod Chung <jarrodchung.dev@gmail.com>
2020-02-14 11:51:20 -0500

    AddReviw & RadioGroup hook based controlled componnent

--- src/components/ratings-and-reviews/RadioGroup.jsx
+++ src/components/ratings-and-reviews/RadioGroup.jsx
@@ -1,28 +1,55 @@
 import React from "react";
 
 const RadioGroup = (props) => {
-  console.log(props);
+  const handleChange = (event) => {
+    props.func(event.target.value);
+  };
   return (
     <div className="control">
       <label className="level">{props.name}</label>
       <label>
-        <input type="radio" value={1} checked={props.val === "1"} onChange={props.func} />
+        <input
+          type="radio"
+          value={1}
+          checked={props.val === "1"}
+          onChange={handleChange}
+        />
         &nbsp; {props.settings[0]} &nbsp;
       </label>
       <label>
-        <input type="radio" value={2} checked={props.val === "2"} onChange={props.func} />
+        <input
+          type="radio"
+          value={2}
+          checked={props.val === "2"}
+          onChange={handleChange}
+        />
         &nbsp; {props.settings[1]} &nbsp;
       </label>
       <label>
-        <input type="radio" value={3} checked={props.val === "3"} onChange={props.func} />
+        <input
+          type="radio"
+          value={3}
+          checked={props.val === "3"}
+          onChange={handleChange}
+        />
         &nbsp; {props.settings[2]} &nbsp;
       </label>
       <label>
-        <input type="radio" value={4} checked={props.val === "4"} onChange={props.func} />
+        <input
+          type="radio"
+          value={4}
+          checked={props.val === "4"}
+          onChange={handleChange}
+        />
         &nbsp; {props.settings[3]} &nbsp;
       </label>
       <label>
-        <input type="radio" value={5} checked={props.val === "5"} onChange={props.func} />
+        <input
+          type="radio"
+          value={5}
+          checked={props.val === "5"}
+          onChange={handleChange}
+        />
         &nbsp; {props.settings[4]} &nbsp;
       </label>
     </div>
